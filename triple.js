e2c41092fc6136cb906b03024f4dcd0c4b96d94d
shohag0007 <merinedrive007@gmail.com>
2024-02-23 22:07:35 +0600

    triple js

---
+++ triple.js
@@ -0,0 +1,11 @@
+const first = 1; // change and check
+const second = true; // change and check
+if(first === second){
+    console.log('values are equal');
+}
+
+else{
+    console.log('values are not equal');
+}
+
+// type coercion or type conversion or type casting.
